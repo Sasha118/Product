a1bceb117ed2fd0db664a526ea52225bf2698757
Sasha118 <122111585+Sasha118@users.noreply.github.com>
2023-03-08 17:14:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,142 +1,27 @@
-*{
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
+let productsGrid = document.getElementById('products-grid');
+let productsArray = [];
+let xhr = new XMLHttpRequest();
+let url = 'https://my-json-server.typicode.com/Sasha118/Product';
+
+xhr.open('GET', url + '/products');
+xhr.responseType = 'json';
+xhr.onload = function() {
+	let products = xhr.response;
+	productsGrid.innerHTML = null;
+	products.forEach(p => {
+		productsArray.push(p);
+		let pElem = document.createElement('div');
+		pElem.classList.add('products');
+		pElem.innerHTML = `
+			<h2 class = 'products-name'>${p.name}</h2>
+			<img class = 'products-photo' src = '${p.photo_url}' alt = '${p.name}'>
+			<p class = 'products-price'><b>Price: </b>${p.price}</p>
+			<p class = 'products-description'><b>Description: </b>${p.description}</p>
+			<a href="profile.html?id=${p.author_id}">Selfer profile</a>
+			<button>Buy</button>
+		`;
+		productsGrid.append(pElem);
+	});
 }
 
-body{
-	font-family: Arial, sans-serif;
-
-}
-
-header, main{
-	margin: 5vh 10vw;
-}
-
-header{
-	display: flex;
-	justify-content: space-between;
-}
-
-#products-grid{
-	display: grid;
-	grid-template-columns: repeat(3, 1fr);
-	grid-gap: 1rem;
-}
-
-.products{
-	background: rgba(0, 0, 0, 0.5);
-	padding: 1rem;
-	border-radius: 1rem;
-	color: #fff;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-}
-
-.products-photo {
-	max-width: 100px;
-	max-height: 100px;
-	box-shadow: 0 0 5px #000;
-	border-radius: 0.5rem;
-	margin: 0.5rem 0;
-	background: #fff;
-}
-
-.products a {
-	color: #fff;
-	margin: 5px 0;
-}
-
-button {
-	cursor: pointer;
-	font-size: 1.4rem;
-	border-radius: 10px;
-	border: none;
-	min-width: 50%;
-}
-
-#cart-button {
-	background: none;
-	border: none;
-	border-radius: 50%;
-	padding: 7px;
-	border: 3px solid #000;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	transition: 0.5s;
-}
-
-#cart-button img {
-	max-width: 32px;
-	max-height: 32px;
-}
-
-#cart-button:hover {
-	background: rgb(170, 170, 255);
-}
-
-#cart {
-	position: relative;
-}
-
-.hide {
-	display: none;
-}
-
-.product button {
-	transition: 0.3s;
-}
-
-.product button:hover {
-	background: rgb(170, 170, 255);
-}
-
-.profile-container{
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	min-height: 70vh;
-	flex-basis: 20rem;
-	text-align: center;
-	border-radius: 1rem;
-}
-
-.profile-img{
-	border-radius: 50%;
-	box-shadow: 1px 1px 5px 0 #000;
-	width: 10rem;
-	height: 10rem;
-	margin: 2rem 0;
-	background: #fff;
-}
-
-.products-link{
-	font-size: 2rem;
-	color: #000;
-	text-decoration: none;
-	transition: 0.3s;
-}
-
-.products-link:hover{
-	transform: scale(1.2);
-}
-
-#profile{
-	background: tomato;
-	padding: 1rem 3rem;
-}
-
-@media (max-width: 1000px) {
-	#product-grid{
-		grid-template-columns: repeat(2, 1fr);
-	}
-}
-
-@media (max-width: 650px) {
-	#product-grid{
-		grid-template-columns: repeat(1, 1fr);
-	}
-}
-
+xhr.send();
